16ccc0daa4005b1a5ebe66825056f7c5ec548efd
Pavle Prica <pavle.prica5@gmail.com>
2022-08-03 16:33:34 +0200

    Add controller

--- database_service/src/main/java/com/zuehlke/database/controller/ServiceReleaseController.java
+++ database_service/src/main/java/com/zuehlke/database/controller/ServiceReleaseController.java
@@ -1,2 +1,28 @@
-package com.zuehlke.database.controller;public class ServiceReleaseController {
+package com.zuehlke.database.controller;
+
+import com.zuehlke.database.model.ServiceRelease;
+import com.zuehlke.database.model.ServiceReleaseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/releases")
+public class ServiceReleaseController {
+
+    private final ServiceReleaseRepository serviceReleaseRepository;
+
+    @PostMapping
+    public ServiceRelease scheduleRelease(@Valid @RequestBody ServiceRelease serviceRelease) {
+        return serviceReleaseRepository.save(serviceRelease);
+    }
+
+    @GetMapping
+    public List<ServiceRelease> getAllReleases() {
+        return serviceReleaseRepository.findAll();
+    }
+
 }
