ab5b4618df938ce31fa3d46109c562f1b8462691
Pavle Prica <pavle.prica5@gmail.com>
2022-08-03 16:33:30 +0200

    Fix model and add validations

--- database_service/src/main/java/com/zuehlke/database/model/ServiceRelease.java
+++ database_service/src/main/java/com/zuehlke/database/model/ServiceRelease.java
@@ -1,8 +1,9 @@
 package com.zuehlke.database.model;
 
-import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 @Entity
@@ -13,13 +14,15 @@ public class ServiceRelease {
     public Long id;
 
     @Column(unique = true, nullable = false)
-    public String releaseName;
-
-    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Release title must not be blank!")
     public String releaseTitle;
 
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Release version must not be blank!")
+    public String releaseVersion;
+
     @Column(nullable = false)
-    @CreationTimestamp
+    @Future(message = "How do you plan to release in the past?")
     public Instant releaseDate;
 
 }
