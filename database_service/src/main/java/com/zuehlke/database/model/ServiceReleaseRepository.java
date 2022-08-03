6d8c10f38efc17403285a2f7289f12fca89908c4
Pavle Prica <pavle.prica5@gmail.com>
2022-08-03 16:17:50 +0200

    Add ServiceRelease repository

---
+++ database_service/src/main/java/com/zuehlke/database/model/ServiceReleaseRepository.java
@@ -0,0 +1,8 @@
+package com.zuehlke.database.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceReleaseRepository extends JpaRepository<ServiceRelease, Long> {
+}
