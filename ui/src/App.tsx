83361200d127753771da10905f2ba8f54f247710
Pavle Prica <pavle.prica5@gmail.com>
2022-08-09 17:17:24 +0200

    Add basic form for releases

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <form>
+                    <label>Release title:</label><br/>
+                    <input type="text" id="releaseTitle" name="releaseTitle" value="First release!"/><br/>
+                    <label>Release version: - (example: v0.0.0)</label><br/>
+                    <input type="text" id="releaseVersion" name="releaseVersion" value="v0.0.1"/><br/>
+                    <label>Release date: - (ISO format)</label><br/>
+                    <input type="text" id="releaseDate" name="releaseDate" value="2022-08-10T00:00:00Z"/><br/>
+                    <input type="submit" value="Schedule release!"/>
+                </form>
+                <h6>(UX/UI you never seen before!)</h6>
+            </header>
+        </div>
+    );
 }
 
 export default App;
